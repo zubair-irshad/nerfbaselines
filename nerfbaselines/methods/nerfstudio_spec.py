01b999e9576766816c6ca7357868e79855658d9b
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2024-10-11 13:12:34 -0400

    fastmap mipnerf360 dataset comparison

--- nerfbaselines/methods/nerfstudio_spec.py
+++ nerfbaselines/methods/nerfstudio_spec.py
@@ -16,30 +16,32 @@ _nerfacto_paper_results = {
     "mipnerf360/bonsai": {"psnr": 32.16, "ssim": 0.933, "lpips_vgg": 0.197},
 }
 
+# if [ "$NERFBASELINES_DOCKER_BUILD" != "1" ]; then
+# conda install -y gcc_linux-64=11 gxx_linux-64=11 make=4.3 cmake=3.28.3 -c conda-forge
+# fi
+# LIBRARY_PATH="$CONDA_PREFIX/lib/stubs" pip install ninja git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
 
 NerfStudioSpec: MethodSpec = {
     "method_class": ".nerfstudio:NerfStudio",
     "conda": {
         "environment_name": os.path.split(__file__[:-len("_spec.py")])[-1].replace("_", "-"),
-        "python_version": "3.10",
+        "python_version": "3.8",
         "install_script": r"""
 conda install -y --override-channels -c nvidia/label/cuda-11.8.0 cuda-toolkit
-conda install -y pytorch==2.3.0 torchvision==0.18.0 pytorch-cuda=11.8 'numpy<2.0.0' -c pytorch -c nvidia
-if [ "$NERFBASELINES_DOCKER_BUILD" != "1" ]; then
-conda install -y gcc_linux-64=11 gxx_linux-64=11 make=4.3 cmake=3.28.3 -c conda-forge
-fi
-LIBRARY_PATH="$CONDA_PREFIX/lib/stubs" pip install ninja git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
-if ! pip install open3d>=0.16.0; then
-    wget -O open3d-0.18.0-py3-none-any.whl https://files.pythonhosted.org/packages/5c/ba/a4c5986951344f804b5cbd86f0a87d9ea5969e8d13f1e8913e2d8276e0d8/open3d-0.18.0-cp311-cp311-manylinux_2_27_x86_64.whl;
-    pip install open3d-0.18.0-py3-none-any.whl;
-    rm -rf open3d-0.18.0-py3-none-any.whl;
-fi
-pip install nerfstudio==0.3.4
-git clone https://github.com/nerfstudio-project/nerfstudio.git
-cd nerfstudio
-git checkout 3a90cb529f893fbf89625a915a53a7a71b97a575
-pip install -e .
-if ! python -c 'import cv2'; then pip install opencv-python-headless; fi
+# conda install -y pytorch==2.3.0 torchvision==0.18.0 pytorch-cuda=11.8 'numpy<2.0.0' -c pytorch -c nvidia
+pip install torch==2.1.2+cu118 torchvision==0.16.2+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
+# pip install ninja git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
+# if ! pip install open3d>=0.16.0; then
+#     wget -O open3d-0.18.0-py3-none-any.whl https://files.pythonhosted.org/packages/5c/ba/a4c5986951344f804b5cbd86f0a87d9ea5969e8d13f1e8913e2d8276e0d8/open3d-0.18.0-cp311-cp311-manylinux_2_27_x86_64.whl;
+#     pip install open3d-0.18.0-py3-none-any.whl;
+#     rm -rf open3d-0.18.0-py3-none-any.whl;
+# fi
+# pip install nerfstudio==0.3.4
+# git clone https://github.com/nerfstudio-project/nerfstudio.git
+# cd nerfstudio
+# git checkout 3a90cb529f893fbf89625a915a53a7a71b97a575
+# pip install -e .
+# if ! python -c 'import cv2'; then pip install opencv-python-headless; fi
 
 function nb-post-install () {
 if [ "$NERFBASELINES_DOCKER_BUILD" = "1" ]; then
